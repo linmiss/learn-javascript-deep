a28c11b473fc8c309be0f8157420afee650bb22b
Linmiss <linmiss8128@gmail.com>
2017-04-09 17:01:19 +0800

    splice

--- lesson-3/Object.js
+++ lesson-3/Object.js
@@ -13,7 +13,7 @@ let colors = ['red', 'green', 'pink', 'white', 'yellow'];
 
 
 let colors2 = colors.slice(1, 2)
-console.log(colors2); //['green']
+//console.log(colors2); //['green']
 
 
 
@@ -24,12 +24,23 @@ console.log(colors2); //['green']
 /**
  * 删除  出入两个参数  第一个是起始位置 第二个是个数
  */
-let colors1 = colors.splice(1, 2);
-console.log(colors1); //['grenn','pink']
+//let colors1 = colors.splice(1, 2);
+//console.log(colors1); //['grenn','pink']
 
 
 /**
  * 插入 3个参数 起始位置 ， 0(要删除的项数) 和 要插入的项
  */
 
-let colors3 = colors.splice(2, 0, '444', '555');
+//let colors3 = colors.splice(2, 0, 'red', 'green');
+//console.log(colors);//[ 'red', 'white', 'red', 'green', 'yellow' ]
+//console.log(colors3); //[]
+
+
+/**
+ *  替换 3个参数 
+ */
+
+let colors4 = colors.splice(2,1,'red','green');
+console.log(colors);//[ 'red', 'green', 'red', 'green', 'white', 'yellow' ]
+console.log(colors4);//[ 'pink' ]

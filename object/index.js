ba57fbbb80effea24baa6e9a1906f7fdbeb63206
linshuang <linshuang@roadclouding.com>
2017-07-03 18:07:14 +0800

    update

---
+++ object/index.js
@@ -0,0 +1,31 @@
+/**
+ * 优化if else
+ */
+
+if (color) {
+  if (color === 'black') {
+    printBlackBackground();
+  } else if (color === 'red') {
+    printRedBackground();
+  } else if (color === 'blue') {
+    printBlueBackground();
+  } else if (color === 'green') {
+    printGreenBackground();
+  } else {
+    printYellowBackground();
+  }
+}
+
+//改进后
+
+let colorObj = {
+  'black': printBlackBackground,
+  'red': printRedBackground,
+  'blue': printBlueBackground,
+  'green': printGreenBackground,
+  'yellow': printYellowBackground
+}
+
+if (color in colorObj) {
+  colorObj[color]();
+}
